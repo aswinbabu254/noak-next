92e05c1d78324b5cec3d3015ba9b6c1a8effc6a5
@aswin_wac_ui <aswin.b@webandcrafts.com>
2024-05-11 18:47:59 +0530

    upates

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,16 @@ import React from "react";
 import { pageData } from "src/api/staticData/homePage";
 import WidgetBlocks from "@/widgets/WidgetBlock";
 
+export function generateMetadata() {
+  return {
+    title: "Noak: Elevate Your Online Presence with Expert Website Design & DevelopmentNoak: Empowering Your Digital Success Through Expert Website Design & Development",
+    description: "Transform your digital presence with Noak – where innovative design meets seamless development. Elevate your online identity with our tailored solutions crafted to captivate and engage your audience. Explore our comprehensive services today.",
+    openGraph: {
+      images: "/images/og-image.png",
+    },
+  };
+}
+
 const HomePage = () => {
   return (
     <>
