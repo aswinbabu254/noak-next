5a2ddaea50e8cbfe44a788b6a6019427a326214b
@aswin_wac_ui <aswin.b@webandcrafts.com>
2024-05-11 19:41:51 +0530

    foixes

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export function generateMetadata() {
     title: "Noak: Elevate Your Online Presence with Expert Website Design & DevelopmentNoak: Empowering Your Digital Success Through Expert Website Design & Development",
     description: "Transform your digital presence with Noak – where innovative design meets seamless development. Elevate your online identity with our tailored solutions crafted to captivate and engage your audience. Explore our comprehensive services today.",
     openGraph: {
-      images: "/images/og-image.png",
+      images: "https://noak.vercel.app/images/og-image.png",
     },
   };
 }
